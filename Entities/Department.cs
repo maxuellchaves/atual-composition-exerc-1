8de26ad97c2036064f7857342081b3db9bac1ac9
Seu Nome <maxuellchaves30@gmail.com>
2020-04-11 17:50:12 -0300

    programa finalizado e executado

---
+++ Entities/Department.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Composição_exer_1.Entities
+{
+    class Department
+    {
+        public  string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Entities/Enums/WorkerLevel.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Composição_exer_1.Entities.Enums
+{
+    enum WorkerLevel : int
+    {
+        Junior =0,
+        MidLevel =1,
+        Senior =2
+
+    }
+}
